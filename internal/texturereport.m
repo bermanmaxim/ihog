784a753eadff7d5daa314061d4b64248cb3925db
Carl Vondrick <vondrick@mit.edu>
2013-02-07 14:11:07 -0500

    added padding for texture report

--- internal/texturereport.m
+++ internal/texturereport.m
@@ -1,7 +1,5 @@
 function texturereport(im, pdnormal, pdtexture),
 
-im = imread(im);
-
 feat = features(im2double(im), 8);
 
 ihognormal = invertHOG(feat, pdnormal);
@@ -13,6 +11,9 @@ im(im < 0) = 0;
 
 ihognormal = repmat(ihognormal, [1 1 3]);
 ihogtexture = repmat(ihogtexture, [1 1 3]);
+
+ihognormal = padarray(ihognormal, [0 10], 1, 'post');
+ihogtexture = padarray(ihogtexture, [0 10], 1, 'post');
 
 graphic = cat(2, ihogtexture, ihognormal, im);
 
